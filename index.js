f3a07738edf0f625ab0dd48a51207cd4db0de614
tylersmith <tyjsmith1@gmail.com>
2023-10-30 15:48:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "Tyler Smith";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = "Welcome, " + firstInitial + "!";
